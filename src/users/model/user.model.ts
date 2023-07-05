e7bc0bb95d4dc04e01d5f411334110fd16f6d9b6
leonardog9823 <guanuco43@gmail.com>
2023-07-05 20:18:53 -0300

    decorators Model

--- src/users/model/user.model.ts
+++ src/users/model/user.model.ts
@@ -1,43 +1,52 @@
-import { Column, DataType, Model, Table } from 'sequelize-typescript';
+import {
+  AllowNull,
+  Column,
+  DataType,
+  Default,
+  Model,
+  PrimaryKey,
+  Table,
+  Unique,
+} from 'sequelize-typescript';
 
 @Table
 export class User extends Model {
+  @PrimaryKey
   @Column({
     type: DataType.INTEGER,
     autoIncrement: true,
-    primaryKey: true,
     unique: true,
   })
   id: number;
 
+  @AllowNull(false)
   @Column({
     type: DataType.STRING(20),
-    allowNull: false,
   })
   name: string;
 
+  @Default('')
   @Column({
     type: DataType.STRING(20),
-    defaultValue: '',
   })
   lastname: string;
 
+  @AllowNull(false)
+  @Unique(true)
   @Column({
     type: DataType.STRING(50),
-    unique: true,
-    allowNull: false,
   })
   email: string;
 
+  @AllowNull(false)
   @Column({
     type: DataType.STRING(50),
-    allowNull: false,
   })
   password: string;
 
+  @Default('')
   @Column({
     type: DataType.TEXT,
-    defaultValue: '',
   })
   imageUrl: string;
 }
