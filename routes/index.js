05f6ec2dde89d3fd432a8faa828084c1a5507675
leonardog9823 <guanuco43@gmail.com>
2023-07-06 03:25:10 -0300

    delete files node/express

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-const userRoute = require("./user");
-const favoriteRoute = require("./favorite");
-const saveRoute = require("./saved");
-
-module.exports = {
-    userRoute,
-    favoriteRoute,
-    saveRoute
-}
